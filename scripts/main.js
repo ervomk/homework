7b500321bf0d803885d7ab52880fd21bbb38d354
Ervo <ervomk@gmail.com>
2015-03-26 19:45:21 +0100

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,31 @@
-$("#field").autocomplete({
-    source: function (request, response) {
-        $.ajax({
-            url: "http://en.wikipedia.org/w/api.php",
-            dataType: "jsonp",
-            data: {
-                'action': "opensearch",
-                'format': "json",
-                'search': request.term
-            },
-            success: function (data) {
-                response(data[1]);
-                $('ul li').addClass('animated fadeIn');
-            }
+
+
+
+(function($) {
+
+    $.fn.autoComplete = function() {
+
+        $(this).keyup(function(){
+            var q = $("#field").val();
+        $.getJSON("http://en.wikipedia.org/w/api.php?callback=?",
+        {
+          srsearch: q,
+          action: "query",
+          list: "search",
+          format: "json"
+        },function(data) {
+            $("#result").empty();
+          $("#result").append("<div class='searchFor'>" +  "Results for <b>" + q + "</b>" + "</div>");
+            $.each(data.query.search, function(i,item){
+           $("#result").append("<div><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title);
         });
+        });
+            
+            
+            
+            
+        });
+
     }
-});
+
+}(jQuery));
